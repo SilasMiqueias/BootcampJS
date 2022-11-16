e4120bcb04837fb712496e649f9c97219991b98b
Silas Miquéias <112900164+SilasMiqueias@users.noreply.github.com>
2022-11-16 10:56:17 -0300

    .

---
+++ aula04/ambiente.js
@@ -0,0 +1,15 @@
+
+function meuNome(nome){
+    console.log(`Meu nome é ${nome}`)
+}
+meuNome('Silas')
+
+function maiorIdade(idade){
+    if(idade >= 18){
+        console.log('Parabéns voçê é maior de idade')
+    }else{
+        console.log('Voçê é menor de idade')
+    }
+}
+
+maiorIdade(19)
